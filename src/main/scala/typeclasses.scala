2d37a1c98d2f8fc857d89fcaa002817b4a962c01
Pawel Szulc <paul.szulc@gmail.com>
2017-09-19 16:20:30 +0100

    Add exercies for types and typeclasses

--- src/main/scala/typeclasses.scala
+++ src/main/scala/typeclasses.scala
@@ -6,38 +6,22 @@ import scalaz._
 
 import Scalaz._
 
+/**
+  * Implement Eq type class
+  */
 object exercise1 {
-  sealed trait PathLike[A] {
-    // ???
-  }
-  object PathLike {
-    def apply[A: PathLike]: PathLike[A] = implicitly[PathLike[A]]
-  }
-}
 
-object exercise2 {
-  import exercise2._
-}
+  /** Before we start, try to reason what is wrong with the code below? */
+  def check(i: Int): String =
+    if(i == "1") "It is one!" else "It is something else then 1"
+  check(1)
+  check(2)
 
-object exercise3 {
-  import exercise1._
-  import exercise2._
+  /** 
+    * 1. Create Eq type class with method equalz checking for equality of two object of type A
+    * 2. Create extension method === 
+    * 3. Create instance for Int
+    * 4. Reimplelemnt check method using Eq
+    */
 
-  sealed trait Node
-  final case object Root extends Node
-  final case class Child(parent: Node, name: String) extends Node
-
-  implicit val NodePathLike: PathLike[Node] = new PathLike[Node] {
-    // ???
-  }
-}
-
-object exercise4 {
-  import exercise1._
-  import exercise2._
-  import exercise3._
-
-  implicit class PathLikeSyntax[A: PathLike](self: A) {
-    // ???
-  }
 }
--- src/main/scala/types.scala
+++ src/main/scala/types.scala
@@ -6,32 +6,48 @@ import scalaz._
 
 import Scalaz._
 
+/** 
+  * Create ADT representing geometric figures using traits and case classes
+  * A figure can be a rectangle or trapezoid or cicrle
+  * Rectangle has side a and side b
+  * Trapezoid has base a, base b and height h
+  * Circle has radious r
+  * 
+  * Can you think of other representation using different machinery?
+  */
 object exercise1 {
-  final case class CheckersBoard(/* ??? */)
-
-  sealed trait CheckerPiece
-  // ???
+  sealed trait Figure
 }
 
+/**
+  * Call me maybe
+  */
 object exercise2 {
-  final case class Box[A](/* ??? */)
+  /** Maybe is of kind *. Change it to be * -> * so that it can hold values of any type */
+  sealed trait Maybe
+  case class Just(value: String)
+  case object Empty
+
+  val gotIt = Just("hello")
+  val nah = Empty
+  // val gotNum = Just(10)
+  // case class User(name: String)
+  // val maybeUser = Just(User("Swift"))
 }
 
+/**
+  * Call each function with any argument so that the code compiles.
+  * Please provide the types explicitly in [ ]
+  */
 object exercise3 {
-  // 1. scala.collection.List
-  // 2. F[_, _]
-  // 3. Option
-  // 4. Int
-  // 5. T[_[_], _]
+  def func1[A](a: A): A = a
+  def func2[F[_], A](f: F[A]): F[A] = f
+  def func3[E[_, _], A, B](e: E[A, B]): E[A, B] = e
+  def func4[T[_[_]], F[_]](t: T[F]): T[F] = t
 }
+
 
 object exercise4 {
-  trait FileSystem {
-    // ???
-  }
-}
-
-object exercise5 {
   sealed trait Example[F[_]] {
     def value: F[String]
   }
@@ -41,4 +57,41 @@ object exercise5 {
     def value: Either[String, Int] = Right(2)
   }
   */
+}
+
+/** Explore the mysteries of magic box */
+object exercise5 {
+
+  /** This is a magic box. */
+  trait MagicBox[A] {
+    type B
+    def create[C](c: C): MagicBox[C]
+    def get: A
+
+    def map(f: A => B): MagicBox[B] = create[B](f(get))
+  }
+
+  /** create a class IntMagicBox[A] that is a MagicBox where B is an Int */
+  // class IntMagicBox[A] ???
+
+  /** 
+    Method transformAndExtract should take a MagicBox and function f, apply to map method and then
+    retrive the value using get method.
+    
+    Implement transformAndExtract
+    */
+  // def transformAndExtract[A](mb: MagicBox[A])(f: A => ???): ??? = ???
+
+  // val strIntMagicBox = new IntMagicBox[String]("hello existential")
+  val length: String => Int = _.length
+  /** call transformAndExtract with instance of intMagicBox and function length to test that it works*/
+  // transformAndExtract[String](strIntMagicBox)(length)
+
+}
+
+
+object exercise51 {
+  trait FileSystem {
+    // ???
+  }
 }
