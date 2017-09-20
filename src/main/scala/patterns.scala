e0a5e000cab31a7c45a2c7c4c19a8b79167645df
Pawel Szulc <paul.szulc@gmail.com>
2017-09-20 12:32:14 +0100

    Added exercise1 and exercise2

--- src/main/scala/patterns.scala
+++ src/main/scala/patterns.scala
@@ -7,6 +7,69 @@ import scalaz._
 import Scalaz._
 
 object exercise1 {
+
+  /** we have some potential errors*/
+  sealed trait Error
+  case object NotInTheMoodToTalkToHumans extends Error
+  case object MiningBitcoinNoTimeToTalk extends Error
+
+  /** this will give you String (but potentially an error as well) */
+  val iWillGiveYouString: Double => Error \/ String =
+    (d: Double) => if(d > 10) d.toString.right else NotInTheMoodToTalkToHumans.left
+
+  /** this will give you Int (but potentially an error as well) */
+  val iWillGiveYouInt: Long => Error \/ Int =
+    (l: Long) => if(l < 0) l.toInt.right else MiningBitcoinNoTimeToTalk.left
+
+    def calculation(d: Double, l: Long): Error \/ (String, Int) = for {
+    s <- iWillGiveYouString(d)
+    i <- iWillGiveYouInt(l)
+  } yield (s, i)
+
+  /** If I'm not interested with an error - I want to ignore it */
+  val result: Option[(String, Int)] = ??? // calculate(20.0, -3)
+
+  /** What is a potential issue with the returned type Error \/ (String, Int) */
+  /** Run all those function calls and reason about the result, what can we do about it? */
+  calculation(20.0, -2)
+  calculation(20.0, 6)
+  calculation(5.0, -2)
+  calculation(5.0, 6)
+
+}
+
+object exercise2 {
+
+  case class User(login: String)
+
+  sealed trait Item
+  case object Apple extends Item
+  case object Orange extends Item
+  /** Basket contains items */
+  case class Basket(items: List[Item])
+
+  /** I have to baskets */
+  val basket1 = Basket(items =  List(Apple, Orange, Apple, Apple))
+  val basket2 = Basket(items =  List(Apple, Apple, Orange))
+
+  /** Could I merge two baskets together */
+  val finalBasket: Basket = ??? // basket1 +  basket2
+
+  val user1 = User("john")
+  val user2 = User("pawel")
+  val user3 = User("jeff")
+
+  /** Could I merge two sessions together */
+  val session1: Map[User, Basket] = Map(user1 -> basket1, user2 -> basket2)
+  val session2: Map[User, Basket] = Map(user1 -> finalBasket, user2 -> basket1, user3 -> basket2)
+
+  val session: Map[User, Basket] = ??? // session1 + session2
+
+  /** Implement toBasket method, what needs to be added for this to work? */
+  def toBasket(maybeItem: Option[Item]): Basket = ???
+}
+
+object exerciseA {
   def readRowCol(): (Int, Int) = {
     println("Please enter a row:")
     val row = readLine()
@@ -16,7 +79,7 @@ object exercise1 {
   }
 }
 
-object exercise2 {
+object exerciseB {
   sealed trait Node
   final case object Root extends Node
   final case class Child(parent: Node, name: String) extends Node
